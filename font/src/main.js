0458bb3b2d7ba0a77a0e43baaeee1c10212677f3
kanous <kanous.sz@flexsystem.com>
2021-04-22 23:50:21 +0800

    refresh font

--- font/src/main.js
+++ font/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import moment from 'moment'
-import axios from 'axios'
-createApp.prototype.$http = axios
+// import axios from 'axios'
+// createApp.prototype.$http = axios
 createApp(App).use(store).use(router).use(moment)
   .mount('#app')

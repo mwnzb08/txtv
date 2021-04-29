3af22189e9dd525e90774e4d3d3b46b52f4106f6
kanous.sz <kanous.sz@flexsystem.com>
2021-04-29 17:47:14 +0800

    md5mix.go 加密密码

---
+++ back/src/txtvGo/config/md5mix.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"strconv"
+)
+
+func Md5MixEncryption (encryps string) string {
+	encryptionAlgorithm :=md5.New()
+	first := len(encryps)
+	var finalNumber int
+	for i := 2 ; i <= first ; i++  {
+		if first%i == 0 {
+			finalNumber = i
+			break
+		}
+	}
+	encryps = strconv.Itoa(first) + encryps + strconv.Itoa(finalNumber)
+	encryptionAlgorithm.Write([]byte(encryps))
+	return hex.EncodeToString(encryptionAlgorithm.Sum(nil))
+}
--- back/src/txtvGo/controllers/login.go
+++ back/src/txtvGo/controllers/login.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kataras/iris/v12/sessions"
 	"main/config"
 	"main/domain"
+	"strings"
 )
 
 func init () {
@@ -26,7 +27,13 @@ func (c *LoginController) Get() string {
 func (c *LoginController) PostLogin() interface{} {
 	params := config.GetJson(c.Ctx) // return map[string]interface{}
 	model := make([]domain.User,0)
-	result := config.SelectDomain(&model, []string{"dpid,pwd"}, params)
+	pwd := params["pwd"]
+	pwd = config.Md5MixEncryption(pwd.(string))
+	delete(params, "pwd")
+	result := config.SelectDomain(&model, []string{"pwd"}, params)
+	if strings.EqualFold(pwd, result) {
+
+	}
 	return result
 }
 

37fdb19dce9cdad56115866ea4f7ca9ffb30b2b6
kanous <moqiue0729@163.com>
2021-05-10 20:41:42 +0800

    router/RouterMapping.json 增加

---
+++ font/src/components/mainPageAssembly/Registry.vue
@@ -0,0 +1,17 @@
+<style scoped lang="stylus">
+
+</style>
+
+<template>
+  <div>
+    <div>欢迎注册</div>
+    <div>用户名</div><a-input></a-input>
+    <div>密码</div><a-input></a-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Registry'
+}
+</script>
